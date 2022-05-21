cf49a137b53fd69176578add5679bd62d2938a9e
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-21 15:52:24 +0300

    add Task001 add Change of firt and last strings in array

---
+++ Task001/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива.
+ */
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0 ; j < n; j++)
+    {
+        array[i,j] = new Random().Next(0, 10);
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+int[] tempArray = new int[n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0 ; j < n; j++)
+    {
+        if ( i == 0)
+        {
+            tempArray[j] = array[0, j];
+            array[0, j] = array[m - 1 , j];
+            array[m - 1, j] = tempArray[j];
+        }
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
